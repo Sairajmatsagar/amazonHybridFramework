0eea6f7b39c8f6537a87061bc965bc6c5989a950
Sairajmatsagar <sairajmatsagar123@gmail.com>
2024-10-13 18:12:08 +0530

    Initial commit

---
+++ testscripts/Test_Login.py
@@ -0,0 +1,33 @@
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture()
+def setup_and_teardown():
+    global driver
+    driver = webdriver.Chrome()
+    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    yield
+    driver.close()
+
+
+@pytest.mark.usefixtures("setup_and_teardown")
+def test_login_with_valid_credentials():
+    driver.find_element(By.NAME, 'username').send_keys("ADMIN")
+    driver.find_element(By.NAME, 'password').send_keys("admin123")
+    driver.find_element(By.XPATH, "//button[@type='submit']").click()
+    assert 'OrangeHRM'.__eq__(driver.title)
+
+
+@pytest.mark.usefixtures("setup_and_teardown")
+def test_login_with_invalid_credentials():
+    driver.find_element(By.NAME, 'username').send_keys("Login")
+    driver.find_element(By.NAME, 'password').send_keys("admin123")
+    driver.find_element(By.XPATH, "//button[@type='submit']").click()
+    error_msg=driver.find_element(By.XPATH,"p[text()='Invalid credentials']")
+    assert "Invalid credentials".__eq__(error_msg.text)
